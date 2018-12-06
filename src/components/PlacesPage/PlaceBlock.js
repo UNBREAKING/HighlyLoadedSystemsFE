41101e234b143d7304828abb121520c139844197
Aleksey Golenkov <Asgolenkov9797@gmail.com>
2018-12-06 03:46:52 +0300

    added styles for PlacesPage

--- src/components/PlacesPage/PlaceBlock.js
+++ src/components/PlacesPage/PlaceBlock.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import { CommonLinkButton } from '../common'
+import { COLORS } from '../../constants'
 
 const PlaceBlock = () => 
   <Wrapper>
@@ -31,15 +32,26 @@ const PlaceBlock = () =>
 export default PlaceBlock
 
 const Wrapper = styled.div`
+  width: 80%;
+  border: 3px solid ${COLORS.black};
+  border-radius: 10px;
+  margin-bottom: 20px;
+  padding: 10px 20px;
+  :hover {
+    border-color: ${COLORS.lightblue};
+  }
 `
 
 const Title = styled.h2`
 `
 
 const Description = styled.p`
+  width: 65%;
 `
 
 const CommonInfoWithLink = styled.div`
+  display: flex;
+  justify-content: space-between;
 `
 
 const Address = styled(Link)`
